7e1b14ae91e466ea670f7c19ec76dd0c15623950
ElJuanes <84542017+ElJuanes@users.noreply.github.com>
2024-09-28 18:51:12 -0500

    Animations

--- InputSystem Test/Assets/Movement.cs
+++ InputSystem Test/Assets/Movement.cs
@@ -10,49 +10,65 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float speed = 5;
     [SerializeField] float jumpForce = 5;
     private bool isGrounded = true;
+    private Animator animator;
     Rigidbody rb;
-
     private void Start()
     {
         playerInput = GetComponent<PlayerInput>();
         moveAction = playerInput.actions.FindAction("Move");
         jumpAction = playerInput.actions.FindAction("Jump");
+
         if (jumpAction == null)
         {
             Debug.LogError("Jump action not found!");
             return;
         }
-        rb = GetComponent<Rigidbody>();
 
-        // Suscríbete al evento de salto aquí en lugar de en OnEnable
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+
         jumpAction.performed += HandleJump;
     }
 
     private void Update()
     {
         MovePlayer();
+        
     }
+
     void MovePlayer()
     {
         Vector2 direction = moveAction.ReadValue<Vector2>();
-        transform.position += new Vector3(direction.x, 0, direction.y) * speed * Time.deltaTime;
+        Vector3 movement = new Vector3(direction.x, 0, direction.y) * speed * Time.deltaTime;
+        transform.position += movement;
+
+        if (movement.magnitude > 0)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
     }
+
     void HandleJump(InputAction.CallbackContext context)
     {
-        // Solo saltar si estamos en el suelo
         if (isGrounded)
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGrounded = false;
+
+            animator.SetBool("isJumping", true);
         }
     }
+
     private void OnCollisionEnter(Collision collision)
     {
-        // Verificar si tocamos el suelo nuevamente
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
         }
+        animator.SetBool("isJumping", false);
     }
-
 }
